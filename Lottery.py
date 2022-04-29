f563f4e755caf0b9bfc5c9ea2d455e6b86eaf16c
erika <ec.silvaalvarado@ugto.mx>
2022-04-29 17:09:31 -0500

    NearestLower esta fallando en casos M,N diferente de 1

--- Lottery.py
+++ Lottery.py
@@ -77,51 +77,108 @@ def MergeArraysPlusIndex(B1,B2, index1, index2):
     return C, index
 
 def SortArray(array, index):
-    L= len(array)
+    L= len(index)
     aS = np.empty(L,dtype=int)
     for i in range(L):
         k = index[i]
         aS[i]=array[k]
     return aS
 
-def LotteryFunction(N, M, A, B, points):
-    # simplest case
-    if M == 0:
-        lottery = np.empty(M, dtype=int)
-        return lottery
+
+
+def NearestLower(A, low, high, key):
+    if high < low:
+        return -1
+    i = math.floor((high-low)/2 + low)
+    aux = A[i]
+    if key == A[i]:
+        return i
+    elif key < A[i]:
+        high = i-1      #search restricted to the lower half of A
+        return NearestLower(A,low,high,key)
+    else: #key > A[i]
+        if A[i+1]>key:
+            return i
+        else:
+            low = i+1       #search restricted to the upper half of A
+            return NearestLower(A, low, high, key)
+
+def SelectSets(A, B, p):
+    # Sorting points array in non-decreasing order with their original indices
+    ps, ip = MergeSortPlusIndex(p, np.array(list(range(M))))
+    # Sorting lower limit array in non-decreasing order
+    As, ia = MergeSortPlusIndex(A, np.array(list(range(len(A)))))
+    # Sorting upper limit array in non-decreasing order
+    Bs, ib = MergeSortPlusIndex(B, np.array(list(range(len(B)))))
+    # Discard sets closing before p[0]
+    posb = NearestLower(Bs, 0, N, p[0])
+    B = Bs[posb + 1:]
+    ib = ib[posb + 1:]
+    # A,B are sorted by B non decremental
+    A = SortArray(A, ib)
+    As, ia2 = MergeSortPlusIndex(A, np.array(list(range(len(A)))))
+    # Discard sets opening after maxPs
+    posa = NearestLower(As, 0, N, ps[- 1])
+    A = As[0:posa + 1]
+    ia = ia2[0:posa + 1]
+    # A,B are sorted by A non decremental
+    B = SortArray(B, ia)
+    return A,B
+
+def LotteryFunction(A, B, p):
+    Na = len(A)
+    Nb = len(B)
+    N = max(Na,Nb)
+    M = len(p)
     if N == 1:  # only one segment
-        lottery = np.empty(M, dtype=int)
-        for j in range(M):
-            NOS = 0
-            if A[0] <= points[j] and B[0] >= points[j]:
-                NOS = 1
-            lottery[j] = NOS
-        return lottery
-    elif M == 1: #only one point
-        NOS=0
-        for i in range(N):
-            if A[i] <= points[0] and B[i] >= points[0]:
-                NOS += 1
-            lottery = NOS
-        return lottery
-    else:
+        lottery = np.zeros(M, dtype=int)
         # Sorting points array in non-decreasing order with their original indices
-        pS, ipS = MergeSortPlusIndex(points, np.array(list(range(M))))
-        # Sorting higher limit array in non-decreasing order
-        Bs, iBs = MergeSortPlusIndex(B, np.array(list(range(N))))
-        Ab = SortArray(A, iBs)
-        lottery = np.empty(M, dtype=int)
-        pos = 0
-        for j in range(M): # voy por cada punto
-            while pos<N and Bs[pos] < pS[j]:#cuento los que cierran antes
-                pos = pos+1
-            Abs = np.sort(Ab[pos:])
-            i = 0
-            while i<M-pos and Ab[pos+i] <=pS[j]:
-                i=i+1
-            k = ipS[j]
-            lottery[k] = i
-        return lottery
+        ps, ip = MergeSortPlusIndex(p, np.array(list(range(M))))
+        # min index points afer A
+        lp = NearestLower(ps, 0, M, A[0])+1
+        # max index points before B
+        rp = NearestLower(ps, 0, M, B[0])
+        for j in range (lp, rp+1):
+            k = ip[j]
+            lottery[k] = 1
+        return True, lottery
+    elif M==1: #one single point
+        lottery = 0
+        #Sort and select sets
+        A,B = SelectSets(A, B, p)
+        lottery = N
+        print(lottery)
+        return True, lottery
+    else:
+        # Sort and select sets
+        A, B = SelectSets(A, B, p)
+        #part B in two halfs
+        halfN = math.floor((N + 1) / 2)
+        B1 = B[0:halfN]
+        B2 = B[halfN:]
+        # Sort points array in non-decreasing order with their original indices
+        ps, ip = MergeSortPlusIndex(p, np.array(list(range(M))))
+        #send points according to the B partitions
+        indexP = NearestLower(p, 0, M - 1, B1[-1])
+        p1 = ps[0:indexP + 1]
+        p2 = ps[indexP + 1:M]
+        # Sort A according to the B partitions
+        indexA = NearestLower(A, 0, N - 1, B1[-1])
+        A1 = A[0:indexA + 1]
+        A2 = A[indexA + 1:]
+        flag1, lottery1 = LotteryFunction(A1, B1, p1)
+        flag2, lottery2 = LotteryFunction(A2, B2, p2)
+        if flag1 == True:
+                if len(A1) > 0:
+                    for j in range(len(p1)):
+                        i = 0
+                        while i < len(A1) and A1[i] <= p1[j]:
+                            count[j] += 1
+                            i += 1
+                        k = ip[lp+j]
+                        lottery[k] = count[j]
+        return True, lottery
+
 
 if __name__ == '__main__':
     input = sys.stdin.read()
@@ -131,7 +188,6 @@ if __name__ == '__main__':
     M = data[1]                 # number of values to find
     A = np.array(data[2:2 * N + 2:2])    # lower limit of each segment
     B = np.array(data[3:2 * N + 2:2])    # upper limit of each segment
-    points = np.array(data[2 * N + 2:])   # points to find
-    results = LotteryFunction(N,M,A,B,points)
-    for x in results:
-        print(x, end=' ')
+    p= np.array(data[2 * N + 2:])   # points to find
+    flag,results = LotteryFunction(A, B, p)
+
